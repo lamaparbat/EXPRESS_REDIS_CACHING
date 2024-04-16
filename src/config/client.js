a98e0c0cba3083a84eaa31844efdcd436a87d6d8
Parbat Lama <lamaparbat70@gmail.com>
2024-04-16 16:10:47 +0545

    Feat: Add Vercel redis KV config

--- src/config/client.js
+++ src/config/client.js
@@ -1,9 +1,13 @@
 const { Redis } = require("ioredis");
 
-const client = new Redis({
-    url: process.env.KV_URL
-});
 
-console.log(process.env.KV_URL)
+console.log({ REDIS: process.env.KV_URL})
+
+const client = new Redis({
+    url: process.env.KV_URL,
+    socket: {
+        tls: true,
+    },
+});
 
 module.exports = client;
